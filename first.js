3f8030f5a2e64eba31160e78f1fde4173bb52268
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-03-06 22:35:05 +0530

    Update first.js

--- first.js
+++ first.js
@@ -5,10 +5,8 @@ function isPalindrome(num) {
     
     let reversedStr = numStr.split('').reverse().join('');
     
-    // Check if the original string is equal to the reversed string
     return numStr === reversedStr;
 }
 
-// Test the function
 console.log(isPalindrome(12321)); // Output: true
 console.log(isPalindrome(12345)); // Output: false
